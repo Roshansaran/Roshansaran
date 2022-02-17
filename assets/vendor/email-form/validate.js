b32ae4e37521e889629bb890c2d8b40149b1e50c
SaranKumar <92871750+Roshansaran@users.noreply.github.com>
2022-02-17 22:12:10 +0530

    Delete assets directory

--- assets/vendor/email-form/validate.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByClassName('email-form')[0].addEventListener('submit', function(e) {
-  e.preventDefault();   
-  var email = this.querySelector('input[type="email"]').value;
-  var name = this.querySelector('input[type="text"]').value;
-  var message = this.querySelector('textarea').value;
-  var link = `mailto:coderz@gces.edu.in?subject=Message from ${name} - [${email}]&body=${message}`;
-  window.location.href = link;
-});
