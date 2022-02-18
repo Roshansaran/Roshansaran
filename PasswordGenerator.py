aaae8f6bd33218f8d5476ba3c33cbd3a5100befd
Saran Kumar <92871750+Roshansaran@users.noreply.github.com>
2022-02-18 09:07:17 +0530

    Password generator using python
    In learn  , start with python my first basic (programming)....

---
+++ PasswordGenerator.py
@@ -0,0 +1,18 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+
+upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+NUMBER = "0123456789"
+
+Symbol = "[]{}#()*; ._-"
+
+all=lower + upper + NUMBER+ Symbol
+
+length = 9
+
+password = "".join(random. sample (all, length))
+
+print(" The Password You Generated is :", password)
+
