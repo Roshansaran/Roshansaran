c575c1cbc24b15c54aa9ff0a921acbbbbb9e174a
Saran Kumar <92871750+Roshansaran@users.noreply.github.com>
2022-02-18 18:54:02 -0800

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -12,7 +12,7 @@ all=lower + upper + NUMBER+ Symbol
 
 length = 9
 
-password = "admin".join(random.sample(all, length))
+password = "".join(random.sample(all, length))
 
 print(" The Password You Generated is :", password)
 
